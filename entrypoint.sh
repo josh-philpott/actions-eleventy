88d2c724ac77556b7dd50a2f7a9dd5816845eeb1
Josh Philpott <jphil529@gmail.com>
2024-01-05 20:07:03 -0700

    install dependencies during eleventy build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+cd $INPUT_DIRECTORY
+
 if [ "$INPUT_INSTALL_DEPENDENCIES" = "true" ]; then
   echo "Running \`npm install\`"
   npm install
 fi
-
-cd $INPUT_DIRECTORY
 
 echo "Running eleventy"
 eleventy $INPUT_ARGS
